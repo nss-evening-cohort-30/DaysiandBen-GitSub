7f2f30ede2167483ed653eecb51ca0998b9ef53f
Ben Johnson <greypilgrim115@gmail.com>
2025-02-11 21:07:25 -0600

    added html with profile, navbar, and their respective divs. Added css styling to profile and navbar. Added data.js

---
+++ data.js
@@ -0,0 +1,120 @@
+let repos = [
+  {
+    id:1,
+    name: 'Repo1',
+    description: 'Here is a description of my repo',
+    language: 'Javascript',
+    keywords: [
+      'Javascript',
+      'React',
+      'Hackerthon'
+    ],
+    pinned: true,
+    type: 'repository'
+  },
+  {
+    id:2,
+    name: 'Repo1',
+    description: 'Here is a description of my repo',
+    language: 'Javascript',
+    keywords: [
+      'Javascript',
+      'React',
+      'Hackerthon'
+    ],
+    pinned: true,
+    type: 'repository'
+  },
+  {
+    id:3,
+    name: 'Repo1',
+    description: 'Here is a description of my repo',
+    language: 'Javascript',
+    keywords: [
+      'Javascript',
+      'React',
+      'Hackerthon'
+    ],
+    pinned: true,
+    type: 'repository'
+  },
+  {
+    id:4,
+    name: 'Repo1',
+    description: 'Here is a description of my repo',
+    language: 'Javascript',
+    keywords: [
+      'Javascript',
+      'React',
+      'Hackerthon'
+    ],
+    pinned: true,
+    type: 'repository'
+  }
+]
+
+let projects = [
+  {
+    id: 1,
+    name: 'Project1',
+    description: 'Here is a description',
+    type: 'project'
+  },
+  {
+    id: 2,
+    name: 'Project1',
+    description: 'Here is a description',
+
+  },
+  {
+    id: 3,
+    name: 'Project1',
+    description: 'Here is a description',
+     type: 'project'
+  },
+  {
+    id: 4,
+    name: 'Project1',
+    description: 'Here is a description',
+     type: 'project'
+  },
+  {
+    id: 5,
+    name: 'Project1',
+    description: 'Here is a description',
+     type: 'project'
+  }
+]
+
+let packages = [
+  {
+    id:1,
+    name: "Package1",
+    description: "Here is a description",
+    linkUrl: 'www.website.com',
+    type: 'package'
+  },
+  {
+    id:1,
+    name: "Package1",
+    description: "Here is a description",
+    linkUrl: 'www.website.com',
+    type: 'package'
+  },
+  {
+    id:1,
+    name: "Package1",
+    description: "Here is a description",
+    linkUrl: 'www.website.com',
+    type: 'package'
+  },
+  {
+    id:1,
+    name: "Package1",
+    description: "Here is a description",
+    linkUrl: 'www.website.com',
+    type: 'package'
+  }
+]
+
+export { repos, projects, packages}
