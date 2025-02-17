3c4d5e3030ff259a7bc5f10b1c41db6c65339137
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-02-17 16:48:05 -0500

    bug

--- overview.js
+++ overview.js
@@ -1,20 +1,53 @@
 import { favorites } from "./data.js";
-import { displayFavorites } from "./packages.js"
-import { displayFavs } from "./repos.js" 
-import { displayFavs } from "./projects.js"
-
 
 const displayInDom = (divID, content) => {
   const findDiv = document.querySelector(divID);
   findDiv.innerHTML = content;
 };
 
+// Create a card
+const createCard = (object) => {
+  return `
+  <div class="card" style="width: 40rem; gap:20px;">
+    <div class="card-body">
+      <h3 class="card-title">${object.name}</h3>
+      <h4 class="card-description">${object.description}</h4>
+      <h5>${object.language}</h5>
+      <h6>${object.type}</h6>
+    </div>
+  </div>`;
+};
 
-let displayOverview = () => {
-    let content = `<h2>Something something</h2>`
-    displayInDom('#main', content)
-}
+const displayCards = (array) => {
+  let content = "";
+  array.forEach((item) => {
+    content += createCard(item);
+  });
 
+  displayInDom("#cards-cnt", content);
+};
 
+//create new Favorite form
+const favForm = document.querySelector("#createProject");
 
-document.querySelector('#overview').addEventListener('click', displayOverview)
+const newPin = (e) => {
+  e.preventDefault();
+  const fav = {
+    id: favorites.length + 1,
+    name: document.querySelector("#name").value,
+    description: document.querySelector("#description").value,
+    language: "Javascript",
+    type: "project",
+    pinned: true,
+  };
+  favorites.push(fav);
+  displayCards(favorites);
+  favForm.reset();
+};
+
+favForm.addEventListener("submit", newPin);
+
+//display cards when the screen loads
+document.addEventListener("DOMContentLoaded", () => {
+  displayCards(favorites);
+});
